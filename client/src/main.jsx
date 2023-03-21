b31da0f4e1e455b88e32c5ff32ae479b453b7138
Thomas Massey <thomassey476@outlook.com>
2023-03-21 20:54:13 +0000

    Testing for outsided the Auth0 provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import Test from './test'
 import './index.css'
 import { Auth0Provider } from '@auth0/auth0-react'
 
@@ -9,6 +10,7 @@ const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Test />
     <Auth0Provider
       domain={domain}
       clientId={clientId}
