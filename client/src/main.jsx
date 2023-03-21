88b7043769c0104d21a055d3ed8baaaa4494e4f8
Thomas Massey <thomassey476@outlook.com>
2023-03-21 22:08:22 +0000

    Working hiden git details

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,21 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import Test from './test'
 import './index.css'
 import { Auth0Provider } from '@auth0/auth0-react'
-import process from 'process'
 
-const domain = process.env.REACT_APP_AUTH0_DOMAIN
-const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
+import domain from './AUTH0_DOMAIN.js'
+import clientId from './AUTH0_CLIENT_ID.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Test />
     <Auth0Provider
       domain={domain}
       clientId={clientId}
-      redirectUri={window.location.origin}
+      authorizationParams={{ redirect_uri: window.location.origin }}
     >
       <App />
     </Auth0Provider>
