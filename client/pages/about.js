c518c18599028f87eaaf1b757a34f4d4f8746603
Thomas Massey <thomassey476@outlook.com>
2023-04-29 23:10:19 +0100

    Added a bit about about

--- client/pages/about.js
+++ client/pages/about.js
@@ -3,9 +3,14 @@ import React from 'react'
 function About() {
   return (
     <div>
-      <h1>About</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      <h1 className='text-9xl font-bold text-center py-20'>
+        About
+      </h1>
+      <h2 className='text-4xl font-semibold text-center py-20'>
+        This website was created for students by students. It's fundermental purpose is to allow students to create their own past papers. This is no easy feat for students so this website aims to make it as easy as possible, creating contributions to the community from the community.<br />
+      </h2>
+      <p className='text-9xl font-semibold text-center py-20'>
+        🤝
       </p>
     </div>
   )
