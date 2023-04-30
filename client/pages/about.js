12f26e31a6a38e00290e090ddf12563a2ee8009c
Thomas Massey <thomassey476@outlook.com>
2023-04-30 11:25:25 +0100

    Changed apostrophe

--- client/pages/about.js
+++ client/pages/about.js
@@ -7,7 +7,7 @@ function About() {
         About
       </h1>
       <h2 className="text-4xl font-semibold text-center py-20">
-        This website was created for students by students. It's fundermental purpose is to allow students to create their own past papers. This is no easy feat for students so this website aims to make it as easy as possible, creating contributions to the community from the community.<br />
+        This website was created for students by students. It s fundermental purpose is to allow students to create their own past papers. This is no easy feat for students so this website aims to make it as easy as possible, creating contributions to the community from the community.<br />
       </h2>
       <p className="text-9xl font-semibold text-center py-20">
         🤝
--- client/pages/index.js
+++ client/pages/index.js
@@ -8,6 +8,7 @@ export default function Home() {
   const total_number_of_paper = 100000
   return (
     <div className="">
+      {/* Make this get smaller depending on the screen size */}
       <h1 className='text-9xl font-bold text-center py-20'>
         PastPaperGen
       </h1>
@@ -16,6 +17,14 @@ export default function Home() {
       </h2>
       <div className='flex justify-center items-center'>
         <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Get Started</button>
+      </div>
+      <div className='flex justify-center items-center'>
+        <p className='text-2xl font-semibold'>
+          Total number of users: {total_number_of_users}
+        </p>
+        <p className='text-2xl font-semibold'>
+          Total number of papers: {total_number_of_paper}
+        </p>
       </div>
     </div>
   )
