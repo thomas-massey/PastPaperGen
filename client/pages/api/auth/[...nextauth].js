813496948e973fd00fb72b1fa3c83043a3f4e9d5
Thomas Massey <thomassey476@outlook.com>
2023-04-30 23:05:09 +0100

    Added instagram auth

--- client/pages/api/auth/[...nextauth].js
+++ client/pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import InstagramProvider from "next-auth/providers/instagram"
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -7,6 +8,10 @@ export const authOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+    }),
+    InstagramProvider({
+      clientId: process.env.INSTAGRAM_CLIENT_ID,
+      clientSecret: process.env.INSTAGRAM_CLIENT_SECRET,
     }),
     // ...add more providers here
   ],
