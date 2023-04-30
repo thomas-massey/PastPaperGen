45a6b53933cc3e1f545a9284a95d823342c045f5
Thomas Massey <thomassey476@outlook.com>
2023-04-30 15:11:14 +0100

    Added contact us formatting

--- client/components/account/Login.js
+++ client/components/account/Login.js
@@ -17,10 +17,12 @@ export default function Login() {
     }
     return (
         <div>
-            <h1 className='text-6xl font-semibold p-9'>
+            <h1 className='text-6xl font-semibold p-9 text-center'>
                 You are currently not signed in. Sign in to continue.
             </h1>
-            <button onClick={() => signIn()}>Sign in</button>
+            <div className='flex justify-center items-center'>
+                <button onClick={() => signIn()} className='p-4 text-6xl font-semibold'>Sign in</button>
+            </div>
         </div>
     )
 }
--- client/pages/contact.js
+++ client/pages/contact.js
@@ -5,37 +5,38 @@ function Contact() {
     return (
         <div>
 
-            <h1>Contact</h1>
-            <form id="contact-form" method="POST">
-                <div className="form-group">
-                    <label htmlFor="name">Name </label>
-                    <input type="text" className="form-control" />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="exampleInputEmail1">Email address </label>
-                    <input type="email" className="form-control" aria-describedby="emailHelp" />
-                </div>
-                <div>
-                    <label htmlFor="subject">Subject </label>
-                    <select className="form-control">
-                        <option>Error with questions</option>
-                        <option>Billing</option>
-                        <option>Account Support</option>
-                    </select>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="message">Message </label>
-                    <textarea className="form-control" rows="5"></textarea>
-                </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
-            <div className="contact-info text-center">
-                <a href="https://www.reddit.com/r/pastpapergen/">
-                    <Image src="/reddit.png" alt="Reddit" width={100} height={100} />
-                </a>
-                <a href="https://twitter.com/pastpapergen">
-                    <Image src="/twitter.png" alt="Twitter" width={100} height={100} />
-                </a>
+            <h1 className='text-6xl font-semibold p-9 text-center'>
+                Contact us
+            </h1>
+            <div className='flex justify-center items-center'>
+                <form id="contact-form" method="POST">
+                    <div className="form-group">
+                        <label htmlFor="name">Name </label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputEmail1">Email address </label>
+                        <input type="email" className="form-control" aria-describedby="emailHelp" />
+                    </div>
+                    <div>
+                        <label htmlFor="subject">Subject </label>
+                        <select className="form-control">
+                            <option>Error with questions</option>
+                            <option>Billing</option>
+                            <option>Account Support</option>
+                        </select>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="message">Message </label>
+                        <textarea className="form-control" rows="5"></textarea>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+            </div>
+            <div className='flex justify-center items-center'>
+                <h1 className='text-6xl font-semibold p-9 text-center'>
+                    Please check our other contacts in our footer!
+                </h1>
             </div>
         </div>
     )
