276753ff18305f5a63edadf97b2a7eb9527b5317
Thomas Massey <thomassey476@outlook.com>
2023-04-30 17:12:29 +0100

    Changed Nabar layout

--- client/components/navbar/Navbar.js
+++ client/components/navbar/Navbar.js
@@ -6,17 +6,22 @@ import Link from 'next/link'
 
 export default function Navbar() {
     return (
-        // Colour of the navbar is ff8400
-        <div className="flex justify-between items-center bg-gradient-to-r bg-ff8400">
+        <div className="flex bg-gradient-to-r bg-ff8400">
             <div className="p-4">
                 <Image src="/logo_transparent.png" alt="Logo" width={100} height={100} />
             </div>
-            <nav className="flex justify-between items-center text-xl font-semibold pr-8">
-                <Link href="/" className='p-4'>Home</Link>
-                <Link href="/about" className='p-4'>About</Link>
-                <Link href="/learn" className='p-4'>Learn</Link>
-                <Link href="/contact" className='p-4'>Contact</Link>
-                <NavbarAccount />
+            <nav className="flex text-2xl">
+                {/* Centre of page */}
+                <div className='flex justify-between items-center'>
+                    <Link href="/" className='p-4'>Home</Link>
+                    <Link href="/about" className='p-4'>About</Link>
+                    <Link href="/learn" className='p-4'>Learn</Link>
+                    <Link href="/contact" className='p-4'>Contact</Link>
+                </div>
+                {/* To the right */}
+                <div className='flex justify-end items-center'>
+                    <NavbarAccount />
+                </div>
             </nav>
         </div>
     )
