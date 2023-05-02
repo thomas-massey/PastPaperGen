d13797bafcfce2a4c7ad5e30b27c6d93b22bf14c
Thomas Massey <76120358+thomas-massey@users.noreply.github.com>
2023-05-02 08:54:25 +0000

    Centred Submit button

--- client/pages/learn/gpt_test.js
+++ client/pages/learn/gpt_test.js
@@ -10,6 +10,8 @@ function gpt_test() {
         <Image src="/test_image.png" alt="test_image" width={500} height={500} />
         <div className="flex flex-row items-center justify-center py-4">
             <textarea id="input" className="border-2 border-black rounded-lg" placeholder="Input"></textarea>
+        </div>
+        <div className="flex flex-row items-center justify-center py-4">
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => {
                 // send input to backend API
                 fetch('/api/learn/gpt_test', {
