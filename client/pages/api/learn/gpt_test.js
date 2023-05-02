2d3fbb5a485032f54bfb97362b278e106f901e1c
Thomas Massey <76120358+thomas-massey@users.noreply.github.com>
2023-05-02 07:57:09 +0000

    Changed from engine to model

--- client/pages/api/learn/gpt_test.js
+++ client/pages/api/learn/gpt_test.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
     console.log("Prompt: " + prompt)
     // Generate the answer
     const gptResponse = await openai.createCompletion({
-        engine: 'davinci',
+        model: 'davinci',
         prompt: prompt,
         maxTokens: 100,
         temperature: 0.9,
