70adb2ecabeb6db64f3b1c2defc6b670c5f2e4dc
Thomas Massey <76120358+thomas-massey@users.noreply.github.com>
2023-05-04 14:17:26 +0000

    Changed syntax

--- client/pages/add.js
+++ client/pages/add.js
@@ -1,36 +1,39 @@
-import React from 'react'
+import React from 'react';
 
 export default function add() {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const name = formData.get('name');
+    const email = formData.get('email');
+    const avatar = formData.get('avatar');
+    const body = { name, email, avatar };
+    const response = await fetch('/api/add', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body),
+    });
+    const result = await response.json();
+    console.log(result);
+  };
+
   return (
     <div>
-        <h1>Add</h1>
-        <form>
-            <label htmlFor="name">Name</label>
-            <input type="text" id="name" name="name" />
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" name="email" />
-            <label htmlFor="avatar">Avatar</label>
-            <input type="text" id="avatar" name="avatar" />
-            <button type="submit"
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                onClick={async (e) => {
-                    e.preventDefault();
-                    const name = e.target.name.value;
-                    const email = e.target.email.value;
-                    const avatar = e.target.avatar.value;
-                    const body = { name, email, avatar };
-                    const response = await fetch('/api/add', {
-                        method: 'POST',
-                        headers: { 'Content-Type': 'application/json' },
-                        body: JSON.stringify(body),
-                    });
-                    const result = await response.json();
-                    console.log(result);
-                }}
-            >
-                Submit
-            </button>
-        </form>
+      <h1>Add</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" name="name" />
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="email" />
+        <label htmlFor="avatar">Avatar</label>
+        <input type="text" id="avatar" name="avatar" />
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Submit
+        </button>
+      </form>
     </div>
-  )
+  );
 }
