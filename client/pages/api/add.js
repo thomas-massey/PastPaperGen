7339bc160446796be198218a32a2640a5b2c3c6c
Thomas Massey <76120358+thomas-massey@users.noreply.github.com>
2023-05-04 11:01:11 +0000

    Added debug statements

--- client/pages/api/add.js
+++ client/pages/api/add.js
@@ -4,6 +4,9 @@ const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
     const { name, email, avatar } = req.body;
+    console.log("Name : ", name);
+    console.log("Email : ", email);
+    console.log("Avatar : ", avatar);
 
     const user = await prisma.user.create({
         data: {
