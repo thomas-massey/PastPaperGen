f2f3335a1b207e9a9b0e6cd958ed0fc7e77d915b
Thomas Massey <76120358+thomas-massey@users.noreply.github.com>
2023-05-05 07:54:13 +0000

    Added notice

--- client/components/navbar/Navbar.js
+++ client/components/navbar/Navbar.js
@@ -5,22 +5,29 @@ import Link from 'next/link'
 
 export default function Navbar() {
     return (
-        <div className="flex justify-between items-center bg-gradient-to-r bg-ff8400">
-            <div className="p-4">
-                <Image src="/logo_transparent.png" alt="Logo" width={100} height={100} />
+        <div>
+            <div className="flex justify-between items-center bg-black">
+                <h1>
+                    This site is still under development ⚒️. Please check back later for updates as we are soon to roll out of beta!
+                </h1>
             </div>
-            <nav className="flex text-2xl font-semibold justify-between items-center">
-                <div className='flex justify-between items-center'>
-                    <Link href="/" className='p-4'>Home</Link>
-                    <Link href="/about" className='p-4'>About</Link>
-                    <Link href="/learn" className='p-4'>Learn</Link>
-                    <Link href="/contact" className='p-4'>Contact</Link>
-                    <Link href="/pricing" className='p-4'>Pricing</Link>
+            <div className="flex justify-between items-center bg-gradient-to-r bg-ff8400">
+                <div className="p-4">
+                    <Image src="/logo_transparent.png" alt="Logo" width={100} height={100} />
                 </div>
-                <div className='flex justify-end items-center'>
-                    <NavbarAccount />
-                </div>
-            </nav>
+                <nav className="flex text-2xl font-semibold justify-between items-center">
+                    <div className='flex justify-between items-center'>
+                        <Link href="/" className='p-4'>Home</Link>
+                        <Link href="/about" className='p-4'>About</Link>
+                        <Link href="/learn" className='p-4'>Learn</Link>
+                        <Link href="/contact" className='p-4'>Contact</Link>
+                        <Link href="/pricing" className='p-4'>Pricing</Link>
+                    </div>
+                    <div className='flex justify-end items-center'>
+                        <NavbarAccount />
+                    </div>
+                </nav>
+            </div>
         </div>
     )
 }
