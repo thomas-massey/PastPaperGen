f0c0ff26de224dcbce685fa7b1c600414b5d995c
Thomas Massey <thomassey476@outlook.com>
2023-05-07 11:53:33 +0100

    Reverted

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -6,54 +6,56 @@ import GoogleProvider from 'next-auth/providers/google';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 
 import prismadb from '@/app/libs/prismadb';
-import { NextApiRequest, NextApiResponse } from 'next';
 
-const options = {
-  adapter: PrismaAdapter(prismadb),
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID as string,
-      clientSecret: process.env.GOOGLE_SECRET as string,
-    }),
-    CredentialsProvider({
-      name: 'credentials',
-      credentials: {
-        email: { label: 'email', type: 'text' },
-        password: { label: 'password', type: 'password' },
-      },
-      async authorize(credentials) {
-        if (!credentials?.email) {
-          throw new Error('Email is required');
-        }
-        if (!credentials?.password) {
-          throw new Error('Password is required');
-        }
-        const user = await prismadb.user.findUnique({
-          where: { email: credentials.email },
-        });
-        if (!user || !user?.hashedPassword) {
-          throw new Error('No user found');
-        }
-        const isValid = await bcrypt.compare(
-          credentials.password,
-          user.hashedPassword
-        );
-        if (!isValid) {
-          throw new Error('Invalid password');
-        }
-        return user;
-      },
-    }),
-  ],
-  debug: process.env.NODE_ENV === 'development',
-  session: {
-    strategy: 'jwt',
-  },
-  secret: process.env.NEXTAUTH_SECRET,
+export const authOptions: AuthOptions = {
+    adapter: PrismaAdapter(prismadb),
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string,
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID as string,
+            clientSecret: process.env.GOOGLE_SECRET as string,
+        }),
+        CredentialsProvider({
+            name: 'credentials',
+            credentials: {
+                email: { label: 'email', type: 'text' },
+                password: { label: 'password', type: 'password' },
+            },
+            async authorize(credentials) {
+                if (!credentials?.email) {
+                    throw new Error('Email is required');
+                }
+                if (!credentials?.password) {
+                    throw new Error('Password is required');
+                }
+                const user = await prismadb.user.findUnique({
+                    where: { email: credentials.email },
+                });
+                if (!user || !user?.hashedPassword) {
+                    throw new Error('No user found');
+                }
+                const isValid = await bcrypt.compare(
+                    credentials.password,
+                    user.hashedPassword
+                );
+                if (!isValid) {
+                    throw new Error('Invalid password');
+                }
+                return user;
+            }
+
+        }),
+    ],
+    debug: process.env.NODE_ENV === 'development',
+    session: {
+        strategy: 'jwt',
+    },
+    secret: process.env.NEXTAUTH_SECRET,
 };
 
-export default (req: AuthOptions | NextApiRequest, res: NextApiResponse) => NextAuth(req, res, options);
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
