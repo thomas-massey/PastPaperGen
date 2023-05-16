b3f38ddfcc480bca974bd20bd703bd07bfc166b3
Thomas Massey <thomassey476@outlook.com>
2023-05-16 22:36:29 +0100

    Changed home

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 const home = () => {
     return (
         <div>
-            <h1>Home</h1>
+            <h1 className="text-4xl font-bold text-center text-gray-800">
+                Welcome to PastPaperGen!
+            </h1>
+            <p className="text-center text-gray-600">
+                This website gives students the power and tools to create their own past papers.
+            </p>
         </div>
     );
 }
