56e5d8638316bf78280e4a67b457a056d6b26713
Thomas Massey <thomassey476@outlook.com>
2023-05-22 00:19:20 +0100

    Fixed footer

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-white py-4 absolute bottom-0 w-full">
+    <footer className="bg-gray-900 text-white py-4">
       <div className="container mx-auto text-center">
         <p className="text-sm">
           © {new Date().getFullYear()} PastPaperGen. All Rights Reserved.
