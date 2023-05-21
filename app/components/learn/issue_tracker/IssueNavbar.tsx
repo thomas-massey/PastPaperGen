d822840fb57392a8c0a8f4408fb9856fd7b6df7c
Thomas Massey <thomassey476@outlook.com>
2023-05-21 13:15:42 +0100

    Added feel lucky

--- app/components/learn/issue_tracker/IssueNavbar.tsx
+++ app/components/learn/issue_tracker/IssueNavbar.tsx
@@ -1,7 +1,25 @@
+import prismadb from '@/app/libs/prismadb';
 import Link from 'next/link';
 
-const IssueTrackerNavbar = () => {
+const IssueTrackerNavbar = async () => {
   // Create, track, feel_lucky as links to the respective pages
+  const number_of_not_closed_issues = await prismadb.issue.count({
+    where: {
+      status: {
+        not: 'CLOSED'
+      }
+    }
+  })
+  const skip = Math.floor(Math.random() * number_of_not_closed_issues)
+  const issue_id = await prismadb.issue.findMany({
+    take: 1,
+    where: {
+      status: {
+        not: 'CLOSED'
+      }
+    },
+    skip: skip
+  })
   return (
     <nav className="bg-gray-900">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -22,7 +40,7 @@ const IssueTrackerNavbar = () => {
               </Link>
             </div>
             <div className="rounded-md bg-gray-800 px-3 py-2">
-              <Link href="/learn/issue_tracker/feel_lucky">
+              <Link href={`/learn/issue_tracker/track/${issue_id[0].simpleId}`}>
                 <div className="text-sm font-medium text-gray-300 hover:text-white focus:outline-none focus:text-white">
                   Feel Lucky
                 </div>
