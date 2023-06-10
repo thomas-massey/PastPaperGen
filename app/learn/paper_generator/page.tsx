6568b58a82fe3b51eb5fabe1411044a23e008f90
Thomas Massey <thomassey476@outlook.com>
2023-06-10 11:33:02 +0100

    Added buttons

--- app/learn/paper_generator/page.tsx
+++ app/learn/paper_generator/page.tsx
@@ -1,6 +1,11 @@
 import LearnNavbar from "@/app/components/learn/LearnNavbar";
 
 const paper_generator = () => {
+    const levels = [
+        "GCSE",
+        "A-Level",
+        "IB"
+    ]
     return (
         <div>
             <LearnNavbar />
@@ -10,6 +15,25 @@ const paper_generator = () => {
             <p className="text-center text-gray-600">
                 Paper Generator is a tool that allows you to create your own past papers.
             </p>
+            {/* What level are you (GCSE, ALEVEL etc) */}
+            <div className="flex flex-col items-center justify-center">
+                <div className="flex flex-col items-center justify-center py-2">
+                    <h1 className="text-2xl font-bold text-center text-gray-800">
+                        What level are you?
+                    </h1>
+                    <p className="text-center text-gray-600">
+                        Select the level of the paper you want to create.
+                    </p>
+                    {levels.map((level) => (
+                        <div className="flex flex-col items-center justify-center">
+                            {/* In the form of a buttton */}
+                            <button className="px-4 py-2 mt-4 text-white bg-blue-500 rounded-md hover:bg-blue-600">
+                                {level}
+                            </button>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 }
