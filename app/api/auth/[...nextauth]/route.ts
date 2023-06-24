3a15b416a4bbe412ea407b9f47fdb2d0bcec0dba
Thomas Massey <thomassey476@outlook.com>
2023-06-24 15:34:08 +0100

    Some mail init

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -6,10 +6,15 @@ import GoogleProvider from 'next-auth/providers/google';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import prismadb from '@/lib/prismadb'
 import generateSimpleId from '@/lib/generateSimpleID';
+import EmailProvider from 'next-auth/providers/email';
 
 const authOptions: AuthOptions = {
     adapter: PrismaAdapter(prismadb),
     providers: [
+        EmailProvider({
+            server: process.env.EMAIL_SERVER,
+            from: process.env.EMAIL_FROM,
+        }),
         GithubProvider({
             clientId: process.env.GITHUB_ID as string,
             clientSecret: process.env.GITHUB_SECRET as string,
